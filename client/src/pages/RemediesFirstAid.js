295040fd0107d9f9ead24c2174dd063c05556768
whooperdars <kimdariuspanis@gmail.com>
2022-10-22 03:35:24 +0800

    remedies aid basic style added

--- client/src/pages/RemediesFirstAid.js
+++ client/src/pages/RemediesFirstAid.js
@@ -6,9 +6,7 @@ const RemediesFirstAid = () => {
   return (
     <section>
         {/* Result section of remedies and first aid */}
-
         <section className='search bar'>
-
         </section>
         <section className='result-container'>
             <div className='title-result'>
@@ -16,7 +14,6 @@ const RemediesFirstAid = () => {
                     <div className='img-overlay'>
                         <img src="" alt="" />
                     </div>
-                    
                 </div>
                  <div>
                     <p>Illness Name</p>
@@ -24,15 +21,29 @@ const RemediesFirstAid = () => {
             
             </div>
             <div className='info-container'>
-
+                <h2>Intro</h2>
+                <div>
+                    description here
+                </div> 
             </div>
             <div className='cause-container'>
-
+                <h2>Causes</h2>
+                <div>
+                    <p>description</p>
+                </div>             
             </div>
-            <div className='info-container'>
-
+            <div className='symptoms-container'>
+                <h2>Symptoms</h2>
+                <div>
+                    <p>description</p>
+                </div>
             </div>
-
+            <div className='instruction-container'>
+                <h2>Instruction</h2>
+                <div>
+                    <p>description</p>
+                </div>
+            </div>
         </section>
     </section>
   )
