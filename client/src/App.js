63202b63149e44e80bffa8f4e9a928ad3efc616a
whooperdars <kimdariuspanis@gmail.com>
2022-10-28 08:00:05 +0800

    lazy loading added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { lazy, Suspense} from 'react'; 
 import io from 'socket.io-client'; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -7,15 +7,17 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 // main pages
-import Homepage from './pages/Homepage';
-import DownloadApp from './pages/DownloadApp';
-import HowItWorks from './pages/HowItWorks';
-import LoginPage from './pages/LoginPage';
-import MedicalBook from './pages/MedicalBook';
-import RemediesFirstAid from './pages/RemediesFirstAid';
-import Pill_Identifier from './pages/Pill_Identifier';
-import SymptomsTracker from './pages/SymptomsTracker';
-import SignUp from './pages/SignUp';
+const Homepage = lazy(() => import('./pages/Homepage')); 
+const DownloadApp = lazy(() => import('./pages/DownloadApp')); 
+const HowItWorks = lazy(() => import('./pages/HowItWorks')); 
+const LoginPage = lazy(() => import('./pages/LoginPage')); 
+const MedicalBook = lazy(() => import('./pages/MedicalBook')); 
+const RemediesFirstAid = lazy(() => import('./pages/RemediesFirstAid')); 
+const Pill_Identifier = lazy(() => import('./pages/Pill_Identifier')); 
+const SymptomsTracker = lazy(() => import('./pages/SymptomsTracker')); 
+const SignUp = lazy(() => import('./pages/SignUp')); 
+
+
 
 const socket = io("http://localhost:4000", { transports: ["websocket"]});
 
@@ -26,21 +28,23 @@ function App() {
       {/* Routes */}
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          {/* Nav bar routes */}
-          <Route path='/' element={<Homepage/>} />
-          <Route path='/how-to-works' element={<HowItWorks/>} />
-          <Route path='/download-app' element={<DownloadApp/>} />
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path='/sign-up' element={<SignUp socket={socket}/>} />
+        <Suspense>
+          <Routes fallback={<div>Loading...</div>}>
+            {/* Nav bar routes */}
+            <Route path='/' element={<Homepage/>} />
+            <Route path='/how-to-works' element={<HowItWorks/>} />
+            <Route path='/download-app' element={<DownloadApp/>} />
+            <Route path='/login' element={<LoginPage/>} />
+            <Route path='/sign-up' element={<SignUp socket={socket}/>} />
 
-          {/* homepage routes */}
-          <Route path='/medical-book' element={<MedicalBook/>} />
-          <Route path='/remedies-and-first-aids' element={<RemediesFirstAid />} />
-          <Route path='/pill-identifier' element={<Pill_Identifier />} />
-          <Route path='/symptoms-tracker' element={<SymptomsTracker />} />
+            {/* homepage routes */}
+            <Route path='/medical-book' element={<MedicalBook/>} />
+            <Route path='/remedies-and-first-aids' element={<RemediesFirstAid />} />
+            <Route path='/pill-identifier' element={<Pill_Identifier />} />
+            <Route path='/symptoms-tracker' element={<SymptomsTracker />} />
 
-        </Routes>
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>    
     </>
